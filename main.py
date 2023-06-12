d9427ece413dd545f1d4672ce7716b050398821f
Fichey <czarekpolk@gmail.com>
2023-06-12 20:12:06 +0200

    added tkinter, began learning

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import tkinter
+import customtkinter
+
+#system settings
+customtkinter.set_appearance_mode("system")
+
+app = customtkinter.CTk()
+
+def button_callback():
+    print('button pressed')
+
+app.title('my app')
+app.geometry("600x400")
+app.grid_columnconfigure(0, weight=1)
+
+button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+app.mainloop()
