db57217f2582dcc5bba5c0040bc7357e547b8309
Fichey <czarekpolk@gmail.com>
2023-06-13 15:03:12 +0200

    added numpy, experimenting with virtualenv

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import tkinter
 import customtkinter
+import numpy
 
-#system settings
-customtkinter.set_appearance_mode("system")
+print("hello world")
 
-app = customtkinter.CTk()
+# customtkinter.set_appearance_mode("system")
 
-def button_callback():
-    print('button pressed')
+# app = customtkinter.CTk()
 
-app.title('my app')
-app.geometry("600x400")
-app.grid_columnconfigure(0, weight=1)
+# def button_callback():
+#     print('button pressed')
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+# app.title('my app')
+# app.geometry("600x400")
+# app.grid_columnconfigure(0, weight=1)
 
-app.mainloop()
+# button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+# button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+# app.mainloop()
+
