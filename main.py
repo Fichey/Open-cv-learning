a62a7980b6c283aeb1739ba83169279c6c88503c
Fichey <czarekpolk@gmail.com>
2023-06-20 20:23:45 +0200

    improved converion and cleaned up code a bit

--- main.py
+++ main.py
@@ -37,57 +37,35 @@ def compress(img: np.uint8, size_of_sqare: int)-> np.uint8:
         return compressed_img
 
 
+# converting img array to ascii characters and printing them
+
 def convert(img: np.uint8):
-    numbers = open("values.txt","r")
-    values = [float(value) for value in numbers.readline().split(", ")]
-    numbers.close()
-    characters = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
-    
-    
-    
-    
-    
-    print(len(characters), len(values))
-    # characters = " .:-=+*#%@"
-    # print(values)
+    characters = " .:-=+*#%@"
+
     for row in img:
         for pixel in row:
-            for value_index in range(len(values)):
-                if values[value_index] >= pixel/255:
-                    print(characters[value_index],end="")
-                    break
+            print(characters[pixel//26],end="")
+            print(characters[pixel//26],end="")
         print()
     
+
+# main app
     
-    
+file_name = input("which file would you like to convert? :")
+compression_level = input("what converion level would you like to use? :")
 
 
-
-img = cv.imread(filename="bird.jpg")
-
+img = cv.imread(filename=file_name)
 if img is None:
     sys.exit("could not read the file.")
 
 
-# copying and converting to gray
-img_clone = img.copy()
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-test = np.arange(36)
-test = test.reshape(6,6)
-print(test[0:2][0:2])
-
-print(img.dtype)
 
 img = compress(img,3)
 
-print(img.shape)
-# print(img_clone.size)
-
-
-
 convert(img)
-    
+
     
 # cv.imshow("Display window", img)
 # k = cv.waitKey(0)
@@ -95,20 +73,3 @@ convert(img)
 # if k == ord("s"):
 #     cv.imwrite("huh.jpg",img)
     
-
-# customtkinter.set_appearance_mode("system")
-
-# app = customtkinter.CTk()
-
-# def button_callback():
-#     print('button pressed')
-
-# app.title('my app')
-# app.geometry("600x400")
-# app.grid_columnconfigure(0, weight=1)
-
-# button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-# button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
-
-# app.mainloop()
-
