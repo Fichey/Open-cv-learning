14d774a9bcfbb95b9fe660317c15ffff9aeb4f2d
Fichey <czarekpolk@gmail.com>
2023-06-29 19:28:00 +0200

    added menu for future developement

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import numpy as np
 import cv2 as cv
 import sys
 
-#getting the average value of the sqare if pixels
 
-def average(cutout: np.uint8):
-    q = 1
-    cutout = cutout.reshape(len(cutout)**2)
-    a = int(sum(cutout) / len(cutout))
-    return a
 
 
 
 #apparently the compress function is built in opencv but I realized this after writing my own so might as well keep it
+    
+    
+#getting the average value of the sqare if pixels
+
+# def average(cutout: np.uint8):
+#     q = 1
+#     cutout = cutout.reshape(len(cutout)**2)
+#     a = int(sum(cutout) / len(cutout))
+#     return a
+    
     
 # def compress(img: np.uint8, size_of_sqare: int)-> np.uint8:
 #     if size_of_sqare < 2:
@@ -49,26 +53,42 @@ def convert(img: np.uint8):
         print()
     
 
+def img_to_asc():
+    file_name = input("which file would you like to convert? :")
+
+    img = cv.imread(filename=file_name)
+    if img is None:
+        sys.exit("could not read the file.")
+        
+    print("***INPUT 0 FOR ORIGINAL IMAGE SIZE***")
+    new_width = int(input("input the desired width of an image: "))
+    new_height = int(input("input the desired height of the image: "))
+
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    if(new_height != 0 and new_width != 0):
+        img = cv.resize(img, (new_width, new_height), interpolation = cv.INTER_CUBIC)
+
+    convert(img)
+
+def vid_to_asc():
+    pass
+
+
 # main app
-    
-file_name = input("which file would you like to convert? :")
 
-img = cv.imread(filename=file_name)
-if img is None:
-    sys.exit("could not read the file.")
-    
-    
-print("***INPUT 0 FOR ORIGINAL IMAGE SIZE***")
-new_width = int(input("input the desired width of an image: "))
-new_height = int(input("input the desired height of the image: "))
+print("Welcome! Please choose what would you like to do by inputing a number")
+print("1. Convert an image into ascii art")
+print("2. Convert a video into ascii art")
+choice = input("")
 
+match choice:
+    case "1":
+        img_to_asc()
+    case "2":
+        vid_to_asc()
     
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-if(new_height != 0 and new_width != 0):
-    img = cv.resize(img, (new_width, new_height), interpolation = cv.INTER_CUBIC)
-
-convert(img)
 
     
 # cv.imshow("Display window", img)
